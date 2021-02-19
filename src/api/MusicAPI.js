13e2fdcbb0a822686a771b97aca782df9191f03f
rorymccann95 <rorymccann95@users.noreply.github.com>
2021-02-19 17:09:22 +0000

    Added api calls for forms and album switching

--- src/api/MusicAPI.js
+++ src/api/MusicAPI.js
@@ -19,6 +19,62 @@ export const getAlbumDetails = (albumId) => {
 }
 
 export const getAlbumSongs = (albumId) => {
-    return  fetch(`http://localhost:8000/api/songs/albumsongs/${albumId}`).then(response => response.json())
+    return fetch(`http://localhost:8000/api/songs/albumsongs/${albumId}`).then(response => response.json())
+}
+
+export const getAlbumOptions = () => {
+    return fetch("http://localhost:8000/api/albums").then(response => response.json())
+}
+
+export const getArtistOptions = () => {
+    return fetch("http://localhost:8000/api/artists").then(response => response.json())
+}
+
+export const getSongOptions = () => {
+    return fetch("http://localhost:8000/api/songs/get/options").then(response => response.json())
+}
+
+export const getAlbumsByArtist = (artistId) => {
+    return fetch(`http://localhost:8000/api/albums/artists/${artistId}`).then(response => response.json())
+}
+
+export const addNewArtist = (artistInfo) => {
+    return fetch("http://localhost:8000/api/artists", {
+        method: 'post',
+        body: JSON.stringify(artistInfo),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response)
+}
+
+export const addNewAlbum = (albumInfo) => {
+    return fetch("http://localhost:8000/api/albums", {
+        method: 'post',
+        body: JSON.stringify(albumInfo),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response)
+}
+
+export const addNewSong = (songInfo) => {
+    return fetch("http://localhost:8000/api/songs", {
+        method: 'post',
+        body: JSON.stringify(songInfo),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response)
+}
+
+export const addNewCollab = (collabInfo) => {
+    return fetch("http://localhost:8000/api/collaborators", {
+        method: 'post',
+        body: JSON.stringify(collabInfo),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response)
 }
 
