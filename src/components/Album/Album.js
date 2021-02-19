0d9fa38201c1cba9afa2268b779be5c6a9bc768e
rorymccann95 <rorymccann95@users.noreply.github.com>
2021-02-19 17:10:44 +0000

    Added switch album functionality and buttons

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { getAlbumDetails, getAlbumSongs } from '../../api/MusicAPI'
+import { getAlbumDetails, getAlbumSongs, getAlbumsByArtist } from '../../api/MusicAPI'
 import { withRouter, useParams } from 'react-router-dom'
 import { getAlbumDuration } from '../../helpers/getAlbumDuration'
 import "./Album.css"
 
-const Album = () => {
+const Album = ({ history }) => {
 
     const [albumHeaderInfo, setAlbumHeaderInfo] = useState([])
     const [albumSongs, setAlbumSongs] = useState([])
@@ -20,29 +20,84 @@ const Album = () => {
     useEffect(() => {
         getAlbumDetails(albumID).then(result => setAlbumHeaderInfo(result))
         getAlbumSongs(albumID).then(result => setAlbumSongs(result))
+
     }, [albumID])
 
+
+    const changeAlbum = ((artist, direction) => {
+        getAlbumsByArtist(artist).then(result => {
+            result.map((nextAlbum) => {
+                const currentAlbum = albumID
+                if (direction === "left") {
+                    if (nextAlbum.id < currentAlbum) {
+                        getAlbumDetails(nextAlbum.id).then(result => {
+                            history.push(`/album/${nextAlbum.id}`)
+                            setAlbumSongs(result)
+                        })
+                    }
+                }
+
+                if (direction === "right") {
+                    if(nextAlbum.id > currentAlbum) {
+                        getAlbumDetails(nextAlbum.id).then(result => {
+                            history.push(`/album/${nextAlbum.id}`)
+                            setAlbumSongs(result)
+                        }) 
+                    }
+                }
+            })
+        })
+    })
+    // if (artist.length > 0) {
+    //     artist.map((nextAlbum, index) => {
+    //         const currentAlbum = albumID
+    //         if (direction === "left") {
+    //             nextAlbum.id < currentAlbum ? getAlbumDetails(nextAlbum.id).then(result => {
+    //                 history.push(`/album/${nextAlbum.id}`)
+    //                 setAlbumSongs(result)
+    //             }) : null
+    //         }
+    //         if (direction === "right") {
+    //             nextAlbum.id > currentAlbum ? getAlbumDetails(nextAlbum.id).then(result => {
+    //                 history.push(`/album/${nextAlbum.id}`)
+    //                 setAlbumSongs(result)
+    //             }) : null
+    //         }
+
+    //     }
+    //     )
+    // }
+    // })
+
     const header = albumHeaderInfo.length > 0 ?
-        <div className="albumHeader">
+        <>
             {albumHeaderInfo.map((album, index) => {
                 return <>
-                    <div className="columnFlex">
-                        <img alt="Album Cover Header" src={album.photoUrl} />
+                    <div className="albumButtons">
+                        <div>
+                            <button className="albumSelector" onClick={() => changeAlbum(album.artistId, "left")}>{"<"}</button>
+                            <button className="albumSelector" onClick={() => changeAlbum(album.artistId, "right")}>{">"}</button>
+                        </div>
                     </div>
-                    <div className="albumDetails">
-                        <h2>{album.albumTitle}</h2>
-                        <h3>{`By: ${album.firstname} ${album.surname}`}</h3>
-                        <h4>{`Description: ${album.description}`}</h4>
-                        <h5>{`Collaborators: ${album.collaborators}`}</h5>
-                        {album.songLengths.includes(',') ? <h5>{`Total Songs: ${(album.songLengths.split(',').length)}`}</h5>
-                            : <h5>Total Songs: 1 </h5>}
-                        {album.songLengths.includes(',') ? <h5>{`Duration: ${getAlbumDuration(album.songLengths.split(','))}`}</h5>
-                            : <h5>{`Duration: ${album.songLengths}`}</h5>}
+                    <div className="albumHeader">
+                        <div key={index} className="columnFlex">
+                            <img alt="Album Cover Header" src={album.photoUrl} />
+                        </div>
+                        <div className="albumDetails">
+                            <h2>{album.albumTitle}</h2>
+                            <h3>{`By: ${album.firstname} ${album.surname}`}</h3>
+                            <h4>{`Description: ${album.description}`}</h4>
+                            <h5>{`Collaborators: ${album.collaborators}`}</h5>
+                            {album.songLengths.includes(',') ? <h5>{`Total Songs: ${(album.songLengths.split(',').length)}`}</h5>
+                                : <h5>Total Songs: 1 </h5>}
+                            {album.songLengths.includes(',') ? <h5>{`Duration: ${getAlbumDuration(album.songLengths.split(','))}`}</h5>
+                                : <h5>{`Duration: ${album.songLengths}`}</h5>}
+                        </div>
                     </div>
                 </>
             })}
 
-        </div>
+        </>
         : null
 
 
